a7822f0f38bd143f453ee8dc37e874817361736c
Camille McCamey <jcmccamey@mail.lipscomb.edu>
2026-09-01 16:21:34 -0500

    Digging Deeper Modified Code
    200 ms and 20% duty cycle

--- EECE3624-Lab02-main/Lab02.asm
+++ EECE3624-Lab02-main/Lab02.asm
@@ -43,7 +43,7 @@ mainLoop:
     cbi PORTC, PORTC3
 
     ; kill some time
-    ldi R16, 40                 ; R16 is outer loop counter
+    ldi R16, 8                 ; R16 is outer loop counter for 40 ms
 outer_loop1:
     ldi R24, low(0x4000)        ; load low and high parts of R25:R24 pair with
     ldi R25, high(0x4000)       ; loop count by loading registers separately
@@ -57,7 +57,7 @@ outer_loop1:
     sbi PORTC, PORTC3
 
     ; kill some time
-    ldi R16, 40                 ; R16 is outer loop counter
+    ldi R16, 32                 ; R16 is outer loop counter for 160 ms
 outer_loop2:
     ldi R24, low(0x4000)        ; load low and high parts of R25:R24 pair with
     ldi R25, high(0x4000)       ; loop count by loading registers separately
